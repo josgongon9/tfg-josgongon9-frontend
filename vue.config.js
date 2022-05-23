82063ef8d5ce2b238edf228e7a3f37c2109c2eed
Jose Miguel <josemiguelgonzalez14@hotmail.com>
2022-05-23 11:20:48 +0200

    url

--- vue.config.js
+++ vue.config.js
@@ -9,13 +9,13 @@ module.exports = {
   proxy: {
     "/api/": process.env.PROXY_API || "http://localhost:8080/"
   },*/
-  /*devServer: {
-    proxy: {
-      "/api/":{
-        target: process.env.PROXY_API || "http://localhost:8080/"
-      } 
-    }
+/*devServer: {
+  proxy: {
+    "/api/":{
+      target: process.env.PROXY_API || "http://localhost:8080/"
+    } 
   }
+}
 
 }*/
 
@@ -23,11 +23,11 @@ module.exports = {
   devServer: {
     // 'auto' | 'all' [string] here
     allowedHosts: 'all',
-    https: true,
+    server: 'https',
     proxy: {
-      "/api/":{
-        target: process.env.PROXY_API || "http://localhost:8080/"
-      } 
+      "/api/": {
+        target: "https://tfg-josgongon9-backend.herokuapp.com/"
+      }
     }
   },
 };
